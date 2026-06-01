9c8975eacf9419b9ac5a0cd5ba3fc383c117dde3
Muhammad Wafiq Afansa <141570068+Xpurple27@users.noreply.github.com>
2026-06-01 17:00:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.getElementById('preloader').classList.add('hide');
+  }, 1900);
+});
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 2,
+    }
+  }
+});
